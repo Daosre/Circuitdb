051305ca47a426c26fef09b24b79f04b0025fc19
Daorse <mmorpgplays5@gmail.com>
2024-10-25 04:56:56 +0200

    Changement de place jwtguard et admin guard car problème pour le role admin

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/Guards';
 import { AdminGuard } from 'src/auth/Guards/admin.guard';
 import { CarService } from './car.service';
 import { CreateCarDto, UpdateCarDto } from './dto';
@@ -21,19 +22,19 @@ export class CarController {
     return this.carService.getAllCar();
   }
 
-  @UseGuards(AdminGuard)
+  @UseGuards(JwtGuard, AdminGuard)
   @Post('/new')
   createCar(@Body() dto: CreateCarDto) {
     return this.carService.addCar(dto);
   }
 
-  @UseGuards(AdminGuard)
+  @UseGuards(JwtGuard, AdminGuard)
   @Patch('/update/:id')
   updateCar(@Body() dto: UpdateCarDto, @Param('id') id: string) {
     return this.carService.updateCar(dto, id);
   }
 
-  @UseGuards(AdminGuard)
+  @UseGuards(JwtGuard, AdminGuard)
   @Delete('/delete/:id')
   deleteCar(@Param('id') id: string) {
     return this.carService.deleteCar(id);
