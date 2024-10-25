a2b059c72764f3b0eb2091a2b9f105078441d463
Daorse <mmorpgplays5@gmail.com>
2024-10-25 18:37:08 +0200

    Correctif

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -14,21 +14,22 @@ import { AdminGuard } from 'src/auth/Guards/admin.guard';
 import { createReservationDto } from './dto';
 import { ReservationService } from './reservation.service';
 
-@UseGuards(JwtGuard)
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
+  @UseGuards(JwtGuard, AdminGuard)
   @Get('/all')
   getAllReservation() {
     return this.reservationService.getAllReservation();
   }
 
+  @UseGuards(JwtGuard)
   @Post('/new')
   createReservation(@Body() dto: createReservationDto) {
     return this.reservationService.addReservation(dto);
   }
 
-  @UseGuards(AdminGuard)
+  @UseGuards(JwtGuard, AdminGuard)
   @Delete('/delete/:id')
   deleteUserReservation(@Param('id') id: string, @GetUser() user: User) {
     return this.reservationService.deleteReservation(user, id);
